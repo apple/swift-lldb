084894c2bef7907cdade9832bea46ba6ed6c95e7
Googler <noreply@google.com>
2018-03-01 17:46:35 -0800

    More CMake build process improvements.
    Developed with danielzheng.
    
    * Add copybara export for lldb, clang, llvm
    * Update synchronization scripts to allow synchronizing multiple projects
    * Add script to synchronize tensorflow
    * Adjust cmake to allow full toolchain build including LLDB
    * build-presets hack to avoid known-bad devices.
    
    PiperOrigin-RevId: 187561810

---
+++ test/testcases/python_api/hello_world/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char const *argv[])
+{
+    lldb_enable_attach();
+
+    printf("Hello world.\n"); // Set break point at this line.
+    if (argc == 1)
+        return 0;
+
+    // Waiting to be attached by the debugger, otherwise.
+    char line[100];
+    while (1) 
+        sleep (1); // Waiting to be attached...
+
+    printf("Exiting now\n");
+}
