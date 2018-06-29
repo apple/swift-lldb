64db6049274ec5c8cc6db9cdc12797ccabaa6965
danielzheng <danielzheng@google.com>
2018-06-29 12:24:59 -0700

    Re-add `ObjectDescription_SummaryProvider` to SwiftFormatters.h.
    `ObjectDescription_SummaryProvider` somehow became lost during rebasing.

--- source/Plugins/Language/Swift/SwiftFormatters.h
+++ source/Plugins/Language/Swift/SwiftFormatters.h
@@ -87,6 +87,10 @@ bool StridedRangeGenerator_SummaryProvider(ValueObject &valobj, Stream &stream,
 bool AccelerateSIMD_SummaryProvider(ValueObject &valobj, Stream &stream,
                                     const TypeSummaryOptions &options);
 
+// SWIFT_ENABLE_TENSORFLOW
+bool ObjectDescription_SummaryProvider(ValueObject &valobj, Stream &stream,
+                                       const TypeSummaryOptions &options);
+
 // TODO: this is a transient workaround for the fact that
 // ObjC types are totally opaque in Swift for LLDB
 bool BuiltinObjC_SummaryProvider(ValueObject &valobj, Stream &stream,
