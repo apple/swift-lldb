apple/swift-lldb

commit 7eba1df14c04ce4ce812e5f0ab8f108899550001

Show original Archetype names in backtraces.
Since this information is now available in DWARF, LLDB can display the
correct names for archetypes when printing backtraces.

<rdar://problem/48259889>

source/Core/Mangled.cpp
@@ -401,7 +401,8 @@ bool Mangled::DemangleWithRichManglingInfo(
 // supplied to this object, or until the end of the object's lifetime.
 //----------------------------------------------------------------------
 const ConstString &
-Mangled::GetDemangledName(lldb::LanguageType language) const {
+Mangled::GetDemangledName(lldb::LanguageType language,
+                          const SymbolContext *sc) const {
   // Check to make sure we have a valid mangled name and that we haven't
   // already decoded our mangled name.
   if (m_mangled && m_demangled.IsNull()) {
@@ -441,7 +442,7 @@ Mangled::GetDemangledName(lldb::LanguageType language) const {
       if (log)
         log->Printf("demangle swift: %s", mangled_name);
       std::string demangled(SwiftLanguageRuntime::DemangleSymbolAsString(
-          mangled_name));
+          mangled_name, false, sc));
       if (!demangled.empty()) {
         m_demangled.SetStringWithMangledCounterpart(demangled,
 						    m_mangled);
@@ -464,8 +465,8 @@ Mangled::GetDemangledName(lldb::LanguageType language) const {
   return m_demangled;
 }
 
-ConstString
-Mangled::GetDisplayDemangledName(lldb::LanguageType language) const {
+ConstString Mangled::GetDisplayDemangledName(lldb::LanguageType language,
+                                             const SymbolContext *sc) const {
   ConstString demangled;
   if (m_mangled) {
     do {
@@ -478,8 +479,9 @@ Mangled::GetDisplayDemangledName(lldb::LanguageType language) const {
               demangled)
             break;
 
-          std::string demangled_std 
-              = SwiftLanguageRuntime::DemangleSymbolAsString(m_mangled, true);
+          std::string demangled_std =
+              SwiftLanguageRuntime::DemangleSymbolAsString(
+                  m_mangled.GetStringRef(), true, sc);
           if (!demangled_std.empty()) {
             demangled.SetCString(demangled_std.c_str());
             display_cache->Insert(mangled, demangled);
@@ -507,11 +509,12 @@ bool Mangled::NameMatches(const RegularExpression &regex,
 // Get the demangled name if there is one, else return the mangled name.
 //----------------------------------------------------------------------
 ConstString Mangled::GetName(lldb::LanguageType language,
-                             Mangled::NamePreference preference) const {
+                             Mangled::NamePreference preference,
+                             const SymbolContext *sc) const {
   if (preference == ePreferMangled && m_mangled)
     return m_mangled;
 
-  ConstString demangled = GetDemangledName(language);
+  ConstString demangled = GetDemangledName(language, sc);
 
   if (preference == ePreferDemangledWithoutArguments) {
     return get_demangled_name_without_arguments(m_mangled, demangled);
@@ -572,15 +575,13 @@ size_t Mangled::MemorySize() const {
 lldb::LanguageType Mangled::GuessLanguage() const {
   ConstString mangled = GetMangledName();
   if (mangled) {
-    if (GetDemangledName(lldb::eLanguageTypeUnknown)) {
-      const char *mangled_name = mangled.GetCString();
-      if (CPlusPlusLanguage::IsCPPMangledName(mangled_name))
-        return lldb::eLanguageTypeC_plus_plus;
-      else if (ObjCLanguage::IsPossibleObjCMethodName(mangled_name))
-        return lldb::eLanguageTypeObjC;
-      else if (SwiftLanguageRuntime::IsSwiftMangledName(mangled_name))
-        return lldb::eLanguageTypeSwift;
-    }
+    const char *mangled_name = mangled.GetCString();
+    if (CPlusPlusLanguage::IsCPPMangledName(mangled_name))
+      return lldb::eLanguageTypeC_plus_plus;
+    else if (ObjCLanguage::IsPossibleObjCMethodName(mangled_name))
+      return lldb::eLanguageTypeObjC;
+    else if (SwiftLanguageRuntime::IsSwiftMangledName(mangled_name))
+      return lldb::eLanguageTypeSwift;
   } else {
     // ObjC names aren't really mangled, so they won't necessarily be in the
     // mangled name slot.

source/Target/SwiftLanguageRuntime.cpp
@@ -85,6 +85,7 @@ using namespace lldb;
 using namespace lldb_private;
 
 static constexpr std::chrono::seconds g_po_function_timeout(15);
+static const char *g_dollar_tau_underscore = u8"$\u03C4_";
 
 namespace lldb_private {
 swift::Type GetSwiftType(void *opaque_ptr) {
@@ -695,26 +696,88 @@ bool SwiftLanguageRuntime::IsSwiftMangledName(const char *name) {
   return swift::Demangle::isSwiftSymbol(name);
 }
 
-std::string SwiftLanguageRuntime::DemangleSymbolAsString (const char *symbol,
-                                                          bool simplified) {
-  if (simplified) {
-    swift::Demangle::DemangleOptions options(swift::Demangle::DemangleOptions::
-                                             SimplifiedUIDemangleOptions());
-    return swift::Demangle::demangleSymbolAsString(
-        symbol, strlen(symbol), options);
-  } else
-    return swift::Demangle::demangleSymbolAsString(symbol, strlen(symbol));
+void SwiftLanguageRuntime::GetArchetypeNamesForFunction(
+    const SymbolContext &const_sc,
+    llvm::DenseMap<SwiftLanguageRuntime::ArchetypePath, StringRef> &dict) {
+  // This terrifying cast avoids having too many differences with llvm.org.
+  SymbolContext &sc = const_cast<SymbolContext &>(const_sc);
+
+  // While building the Symtab itself the symbol context is incomplete.
+  if (!sc.function && sc.module_sp && sc.symbol) {
+    SymbolContextList sc_list;
+    bool symbols_okay = false;
+    bool inlines_okay = false;
+    bool append = false;
+    size_t num_matches = sc.module_sp->FindFunctions(
+        sc.symbol->GetMangled().GetMangledName(), nullptr,
+        lldb::eFunctionNameTypeBase, inlines_okay, symbols_okay, append,
+        sc_list);
+    for (size_t idx = 0; idx < num_matches; idx++) {
+      sc_list.GetContextAtIndex(idx, sc);
+      if (sc.function)
+        break;
+    }
+  }
+
+  Block *block = sc.GetFunctionBlock();
+  if (!block)
+    return;
+
+  bool can_create = true;
+  VariableListSP var_list = block->GetBlockVariableList(can_create);
+  if (!var_list)
+    return;
+
+  for (unsigned i = 0; i < var_list->GetSize(); ++i) {
+    VariableSP var_sp = var_list->GetVariableAtIndex(i);
+    StringRef name = var_sp->GetName().GetStringRef();
+    if (!name.consume_front(g_dollar_tau_underscore))
+      continue;
+
+    uint64_t depth;
+    if (name.consumeInteger(10, depth))
+      continue;
+
+    if (!name.consume_front("_"))
+      continue;
+
+    uint64_t index;
+    if (name.consumeInteger(10, index))
+      continue;
+
+    if (!name.empty())
+      continue;
+
+    Type *archetype = var_sp->GetType();
+    if (!archetype)
+      continue;
+
+    dict.insert({{depth, index}, archetype->GetName().GetStringRef()});
+  }
 }
 
-std::string SwiftLanguageRuntime::DemangleSymbolAsString (const ConstString &symbol,
-                                                          bool simplified) {
-  if (simplified) {
-    swift::Demangle::DemangleOptions options(swift::Demangle::DemangleOptions::
-                                             SimplifiedUIDemangleOptions());
-    return swift::Demangle::demangleSymbolAsString(
-        symbol.GetStringRef(), options);
-  } else
-    return swift::Demangle::demangleSymbolAsString(symbol.GetStringRef());
+std::string
+SwiftLanguageRuntime::DemangleSymbolAsString(StringRef symbol, bool simplified,
+                                             const SymbolContext *sc) {
+  bool did_init = false;
+  llvm::DenseMap<ArchetypePath, StringRef> dict;
+  swift::Demangle::DemangleOptions options;
+  if (simplified)
+    options = swift::Demangle::DemangleOptions::SimplifiedUIDemangleOptions();
+
+  if (sc) {
+    options.ArchetypeName = [&](uint64_t index, uint64_t depth) {
+      if (!did_init) {
+        GetArchetypeNamesForFunction(*sc, dict);
+        did_init = true;
+      }
+      auto it = dict.find({depth, index});
+      if (it != dict.end())
+        return it->second.str();
+      return swift::Demangle::archetypeName(index, depth);
+    };
+  }
+  return swift::Demangle::demangleSymbolAsString(symbol, options);
 }
 
 bool SwiftLanguageRuntime::IsSwiftClassName(const char *name)
@@ -2281,9 +2344,8 @@ bool SwiftLanguageRuntime::FixupReference(lldb::addr_t &addr,
 }
 
 bool SwiftLanguageRuntime::IsRuntimeSupportValue(ValueObject &valobj) {
-  llvm::StringRef g_dollar_tau(u8"$\u03C4_");
   auto valobj_name = valobj.GetName().GetStringRef();
-  if (valobj_name.startswith(g_dollar_tau))
+  if (valobj_name.startswith(g_dollar_tau_underscore))
     return true;
 
   auto valobj_type_name = valobj.GetTypeName().GetStringRef();

include/lldb/Core/Mangled.h
@@ -176,7 +176,9 @@ public:
   /// @return
   ///     A const reference to the demangled name string object.
   //----------------------------------------------------------------------
-  const ConstString &GetDemangledName(lldb::LanguageType language) const;
+  const ConstString &
+  GetDemangledName(lldb::LanguageType language,
+                   const SymbolContext *sc = nullptr) const;
 
   //----------------------------------------------------------------------
   /// Display demangled name get accessor.
@@ -184,7 +186,8 @@ public:
   /// @return
   ///     A const reference to the display demangled name string object.
   //----------------------------------------------------------------------
-  ConstString GetDisplayDemangledName(lldb::LanguageType language) const;
+  ConstString GetDisplayDemangledName(lldb::LanguageType language,
+                                      const SymbolContext *sc = nullptr) const;
 
   void SetDemangledName(const ConstString &name) { m_demangled = name; }
 
@@ -218,7 +221,8 @@ public:
   ///     other name is returned.
   //----------------------------------------------------------------------
   ConstString GetName(lldb::LanguageType language,
-                      NamePreference preference = ePreferDemangled) const;
+                      NamePreference preference = ePreferDemangled,
+                      const SymbolContext *sc = nullptr) const;
 
   //----------------------------------------------------------------------
   /// Check if "name" matches either the mangled or demangled name.

include/lldb/Symbol/CompilerType.h
@@ -172,7 +172,7 @@ public:
 
   ConstString GetTypeName() const;
 
-  ConstString GetDisplayTypeName(lldb::StackFrameSP = {}) const;
+  ConstString GetDisplayTypeName(const SymbolContext *sc = nullptr) const;
 
   ConstString GetTypeSymbolName() const;
 

include/lldb/Symbol/Function.h
@@ -516,11 +516,11 @@ public:
   //------------------------------------------------------------------
   const DWARFExpression &GetFrameBaseExpression() const { return m_frame_base; }
 
-  ConstString GetName() const;
+  ConstString GetName(const SymbolContext *sc = nullptr) const;
 
-  ConstString GetNameNoArguments() const;
+  ConstString GetNameNoArguments(const SymbolContext *sc = nullptr) const;
 
-  ConstString GetDisplayName() const;
+  ConstString GetDisplayName(const SymbolContext *sc = nullptr) const;
 
   const Mangled &GetMangled() const { return m_mangled; }
 

include/lldb/Symbol/SwiftASTContext.h
@@ -536,8 +536,7 @@ public:
 
   ConstString GetTypeName(void *type) override;
 
-  ConstString GetDisplayTypeName(void *type,
-                                 lldb::StackFrameSP frame_sp) override;
+  ConstString GetDisplayTypeName(void *type, const SymbolContext *sc) override;
 
   ConstString GetTypeSymbolName(void *type) override;
 

include/lldb/Symbol/SymbolContext.h
@@ -181,7 +181,7 @@ public:
                        const Address &so_addr, bool show_fullpaths,
                        bool show_module, bool show_inlined_frames,
                        bool show_function_arguments,
-                       bool show_function_name) const;
+                       bool show_function_name);
 
   //------------------------------------------------------------------
   /// Get the address range contained within a symbol context.

include/lldb/Symbol/TypeSystem.h
@@ -227,8 +227,9 @@ public:
 
   // Defaults to GetTypeName(type).  Override if your language desires
   // specialized behavior.
+  // \param sc  An optional symbol context of the function the type appears in.
   virtual ConstString GetDisplayTypeName(lldb::opaque_compiler_type_t type,
-                                         lldb::StackFrameSP frame_sp = {});
+                                         const SymbolContext *sc = nullptr);
 
   // Defaults to GetTypeName(type).  Override if your language desires
   // specialized behavior.

commit 6f74b186fbebbf0d561339044f81c7175029f031

Avoid querying funciton debug info while building the symbol table.
This fixes a testcase breakage introduced by 7eba1df14c04ce4ce812e5f0ab8f108899550001.

rdar://problem/48580111

source/Core/Mangled.cpp
@@ -443,6 +443,9 @@ Mangled::GetDemangledName(lldb::LanguageType language,
         log->Printf("demangle swift: %s", mangled_name);
       std::string demangled(SwiftLanguageRuntime::DemangleSymbolAsString(
           mangled_name, false, sc));
+      // Don't cache the demangled name the function isn't available yet.
+      if (!sc || !sc->function)
+        return ConstString(demangled);
       if (!demangled.empty()) {
         m_demangled.SetStringWithMangledCounterpart(demangled,
 						    m_mangled);

source/Target/SwiftLanguageRuntime.cpp
@@ -702,21 +702,10 @@ void SwiftLanguageRuntime::GetGenericParameterNamesForFunction(
   SymbolContext &sc = const_cast<SymbolContext &>(const_sc);
 
   // While building the Symtab itself the symbol context is incomplete.
-  if (!sc.function && sc.module_sp && sc.symbol) {
-    SymbolContextList sc_list;
-    bool symbols_okay = false;
-    bool inlines_okay = false;
-    bool append = false;
-    size_t num_matches = sc.module_sp->FindFunctions(
-        sc.symbol->GetMangled().GetMangledName(), nullptr,
-        lldb::eFunctionNameTypeBase, inlines_okay, symbols_okay, append,
-        sc_list);
-    for (size_t idx = 0; idx < num_matches; idx++) {
-      sc_list.GetContextAtIndex(idx, sc);
-      if (sc.function)
-        break;
-    }
-  }
+  // Note that calling sc.module_sp->FindFunctions() here is too early and
+  // would mess up the loading process.
+  if (!sc.function && sc.module_sp && sc.symbol)
+    return;
 
   Block *block = sc.GetFunctionBlock();
   if (!block)