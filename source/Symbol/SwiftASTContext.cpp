0080066433782b77312fe9f05ed24ce1f669b90a
Adrian Prantl <aprantl@apple.com>
2019-10-14 15:15:47 -0700

    Extract additional triple details from serialized Swift ASTs.
    Unfortunately things like the vendor name can affect Swift's ability
    to find the correct stdlib.
    
    <rdar://problem/45766181>

--- source/Symbol/SwiftASTContext.cpp
+++ source/Symbol/SwiftASTContext.cpp
@@ -1827,6 +1827,7 @@ lldb::TypeSystemSP SwiftASTContext::CreateInstance(lldb::LanguageType language,
     }
   }
 
+  triple = swift_ast_sp->GetTriple();
   StringRef resource_dir = swift_ast_sp->GetResourceDir(triple);
   ConfigureResourceDirs(swift_ast_sp->GetCompilerInvocation(),
                         FileSpec(resource_dir), triple);
@@ -2039,7 +2040,6 @@ lldb::TypeSystemSP SwiftASTContext::CreateInstance(lldb::LanguageType language,
   if (target.GetArchitecture().IsFullySpecifiedTriple()) {
     swift_ast_sp->SetTriple(target.GetArchitecture().GetTriple());
   } else {
-    // Always run using the Host OS triple...
     bool set_triple = false;
     PlatformSP platform_sp(target.GetPlatform());
     llvm::Triple target_triple = target.GetArchitecture().GetTriple();
@@ -2064,7 +2064,7 @@ lldb::TypeSystemSP SwiftASTContext::CreateInstance(lldb::LanguageType language,
       }
   }
 
-  llvm::Triple triple(swift_ast_sp->GetTriple());
+  llvm::Triple triple = swift_ast_sp->GetTriple();
   StringRef resource_dir = swift_ast_sp->GetResourceDir(triple);
   ConfigureResourceDirs(swift_ast_sp->GetCompilerInvocation(),
                         FileSpec(resource_dir), triple);
